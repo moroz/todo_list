b1f57f9398fb86086fa3caba84ca56c27729c549
Karol Moroz <k.j.moroz@gmail.com>
2020-04-24 11:11:21 +0800

    Set up ApolloProvider

---
+++ assets/js/TodoApp/client.ts
@@ -0,0 +1,5 @@
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({ uri: "/api" });
+
+export default client;
--- assets/js/TodoApp/index.tsx
+++ assets/js/TodoApp/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import client from "./client";
+import { ApolloProvider } from "@apollo/react-hooks";
 
 const TodoApp = () => {
-  return <h1>Hello from React!</h1>;
+  return (
+    <ApolloProvider client={client}>
+      <h1>Hello from React!</h1>
+    </ApolloProvider>
+  );
 };
 
 export default TodoApp;
